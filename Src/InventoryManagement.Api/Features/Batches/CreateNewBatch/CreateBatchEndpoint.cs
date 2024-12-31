bc7e9e6a3ef4353df2ec9f8e8763a1c15296f9d7
Yashoja Lakmith <yashoja002@gmail.com>
2024-12-31 20:52:33 +0530

    Implemented Create Batch feature

--- Src/InventoryManagement.Api/Features/Batches/CreateNewBatch/CreateBatchEndpoint.cs
+++ Src/InventoryManagement.Api/Features/Batches/CreateNewBatch/CreateBatchEndpoint.cs
@@ -1,5 +1,6 @@
 ﻿using FluentResults;
 
+using InventoryManagement.Api.Errors;
 using InventoryManagement.Api.Features.Users;
 using InventoryManagement.Api.Utilities;
 
@@ -13,17 +14,22 @@ public class CreateBatchEndpoint : IEndpoint
 {
     public void MapEndpoint(IEndpointRouteBuilder routeBuilder)
     {
-        routeBuilder.MapPost(@"/api/v1/batch/", async (
+        routeBuilder.MapPost(
+            @"/api/v1/batch/",
+            async (
             [FromBody] NewBatchInformation newBatchInformation,
             ISender sender) =>
         {
             return await CreateNewBatchAsync(newBatchInformation, sender);
         })
-            .RequireAuthorization(policy => policy.RequireRole([Roles.ScheduleManager]))
-            .WithBatchEndpointName(BatchEndpointNameConstants.CreateBatch);
+            .RequireAuthorization(policy => policy.RequireRole(Roles.SuperUser, Roles.ScheduleManager))
+            .WithBatchEndpointName(BatchEndpointNameConstants.CreateBatch)
+            .Produces(StatusCodes.Status201Created)
+            .Produces<List<IError>>(StatusCodes.Status400BadRequest)
+            .Produces(StatusCodes.Status500InternalServerError);
     }
 
-    public static async Task<IResult> CreateNewBatchAsync(NewBatchInformation newBatchInformation, ISender sender)
+    private static async Task<IResult> CreateNewBatchAsync(NewBatchInformation newBatchInformation, ISender sender)
     {
         Result requestResult = await sender.Send(newBatchInformation);
 
@@ -34,6 +40,19 @@ public class CreateBatchEndpoint : IEndpoint
 
     private static IResult MatchErrors(Result requestResult)
     {
-        throw new NotImplementedException();
+        if (requestResult.HasError<InvalidDataError>())
+        {
+            return Results.BadRequest(requestResult.Errors);
+        }
+        if (requestResult.HasError<AlreadyExistsError>())
+        {
+            return Results.BadRequest(requestResult.Errors);
+        }
+        if (requestResult.HasError<NotFoundError>())
+        {
+            return Results.BadRequest(requestResult.Errors);
+        }
+
+        return Results.InternalServerError();
     }
 }
