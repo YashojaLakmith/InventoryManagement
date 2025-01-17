77ee1b8602005e402b4624aff7c6d483cbe52d61
Yashoja Lakmith <yashoja002@gmail.com>
2025-01-18 00:12:56 +0530

    Created batch number and item number validators to be used as shared validators

---
+++ Src/InventoryManagement.Api/Features/Shared/Validators/BatchNumberValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace InventoryManagement.Api.Features.Shared.Validators;
+
+public class BatchNumberValidator : AbstractValidator<BatchNumber>
+{
+    public BatchNumberValidator()
+    {
+        RuleFor(batchNumber => batchNumber.Value)
+            .NotEmpty()
+            .WithMessage(@"Batch number is required.")
+            .Matches(@"^(?!_)[A-Z0-9_]{1,25}(?<!_)$")
+            .WithMessage(@"Batch number length should be less than 25 characters. Batch number must only contain uppercase letters, numbers and underscores and underscores must not be placed at the begining or end of the batch number.");
+    }
+}
+
+public readonly struct BatchNumber(string batchNumber)
+{
+    public readonly string Value = batchNumber;
+}
---
+++ Src/InventoryManagement.Api/Features/Shared/Validators/ItemNumberValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+
+namespace InventoryManagement.Api.Features.Shared.Validators;
+
+public class InventoryItemNumberValidator : AbstractValidator<InventoryItemNumber>
+{
+    public InventoryItemNumberValidator()
+    {
+        RuleFor(itemNumber => itemNumber.Value)
+            .NotEmpty()
+            .WithMessage(@"Item number is required.")
+            .Matches(@"^(?!_)[A-Z0-9_]{1,25}(?<!_)$")
+            .WithMessage(@"Item number length should be less than 25 characters. Item number must only contain uppercase letters, numbers and underscores and underscores must not be placed at the begining or end of the item number.");
+    }
+}
+
+public readonly struct InventoryItemNumber(string inventoryItemNumber)
+{
+    public readonly string Value = inventoryItemNumber;
+}
