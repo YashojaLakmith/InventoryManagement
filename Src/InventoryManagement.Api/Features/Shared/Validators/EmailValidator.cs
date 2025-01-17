a0181e8560e6c8c78b1bbddd0b309e90152373c4
Yashoja Lakmith <yashoja002@gmail.com>
2025-01-17 23:35:43 +0530

    Introduced shared email and password validators

---
+++ Src/InventoryManagement.Api/Features/Shared/Validators/EmailValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace InventoryManagement.Api.Features.Shared.Validators;
+
+public class EmailValidator : AbstractValidator<Email>
+{
+    private const string InvalidEmailMessage = @"Provided email is not valid.";
+
+    public EmailValidator()
+    {
+        RuleFor(email => email.EmailAddress)
+            .NotNull()
+            .WithMessage(InvalidEmailMessage)
+            .MaximumLength(256)
+            .WithMessage(InvalidEmailMessage)
+            .Matches(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")
+            .WithMessage(InvalidEmailMessage);
+    }
+}
+
+public readonly struct Email(string email)
+{
+    public readonly string EmailAddress = email;
+}
