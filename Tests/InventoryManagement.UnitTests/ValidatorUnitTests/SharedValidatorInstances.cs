05e88ded72b2fc22d21eff8485f36d5c55d3feab
Yashoja Lakmith <yashoja002@gmail.com>
2025-01-18 19:04:19 +0530

    Set up shared validator instances for unit testing

---
+++ Tests/InventoryManagement.UnitTests/ValidatorUnitTests/SharedValidatorInstances.cs
@@ -0,0 +1,12 @@
+﻿using InventoryManagement.Api.Features.Shared.Validators;
+
+namespace InventoryManagement.UnitTests.ValidatorUnitTests;
+internal static class SharedValidatorInstances
+{
+    public static readonly BatchNumberValidator BatchNumberValidator = new();
+    public static readonly EmailValidator EmailValidator = new();
+    public static readonly PasswordValidator PasswordValidator = new();
+    public static readonly InventoryItemNumberValidator InventoryItemNumberValidator = new();
+    public static readonly RoleNameValidator RoleNameValidator = new();
+    public static readonly UserIdValidator UserIdValidator = new();
+}
