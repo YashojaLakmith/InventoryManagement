8dc8c27aef945b7da6a4ebaeeb6404363c4f7f98
Yashoja Lakmith <yashoja002@gmail.com>
2025-01-30 20:35:26 +0530

    Fixed a bug in transaction report filter vaidator which caused null transaction type names to be continued with validation.

--- Src/InventoryManagement.Api/Features/Transactions/CreateTransactionReport/TransactionReportFilterValidator.cs
+++ Src/InventoryManagement.Api/Features/Transactions/CreateTransactionReport/TransactionReportFilterValidator.cs
@@ -11,25 +11,33 @@ public class TransactionReportFilterValidator : AbstractValidator<TransactionRep
         RuleFor(info => info.SinceDate)
             .NotNull()
             .WithMessage(@"From date is required.")
-            .LessThanOrEqualTo(timeProvider.CurrentUtcTime)
+            .LessThan(timeProvider.CurrentUtcTime)
             .WithMessage(@"From date must not be a future date.");
 
         RuleFor(info => info.ToDate)
             .NotNull()
             .WithMessage(@"To date is required.")
-            .GreaterThan(timeProvider.CurrentUtcTime)
-            .WithMessage(@"From date must not be a past date.");
+            .LessThanOrEqualTo(timeProvider.CurrentUtcTime)
+            .WithMessage(@"To date must not be a future date.");
+
+        RuleFor(info => info.SinceDate < info.ToDate)
+            .Equal(true)
+            .WithMessage("To date must be greate than since date.");
 
         RuleFor(info => info.TransactionTypes)
             .NotNull()
             .WithMessage(@"Transaction types should not be null.");
 
+        RuleFor(info => info.TransactionTypes)
+            .Must(types => !types.Distinct().Any())
+            .WithMessage("Transaction types contain duplicate items.")
+            .When(info => info.TransactionTypes != null);
+
         RuleForEach(info => info.TransactionTypes)
-            .NotEmpty()
-            .WithMessage(@"Transaction type name should not be null or empty.")
             .Must(name =>
-                name.Equals(InventoryTransactionTypes.Issue, StringComparison.OrdinalIgnoreCase)
-                || name.Equals(InventoryTransactionTypes.Receive, StringComparison.OrdinalIgnoreCase))
-            .WithMessage(@"Invalid transaction type.");
+                name != null
+                && (name.Equals(InventoryTransactionTypes.Issue, StringComparison.OrdinalIgnoreCase)
+                    || name.Equals(InventoryTransactionTypes.Receive, StringComparison.OrdinalIgnoreCase)))
+            .WithMessage(@"Invalid transaction type name.");
     }
 }
